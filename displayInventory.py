e1b8aa986f11332b04392144bede74431b0ac3d3
Tyler Paneitz <Tyler.Paneitz@vu.com>
2022-04-05 12:13:11 -0500

    Changed from fixed input to inventory input

--- displayInventory.py
+++ displayInventory.py
@@ -3,9 +3,9 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 def displayInventory(inventory):
     total = 0
     print("Inventory:")
-    for key in stuff:
-        total += stuff[key]
-        print(str(stuff[key]) + " " + key)
+    for key in inventory:
+        total += inventory[key]
+        print(str(inventory[key]) + " " + key)
     print("Total number of items: " + str(total))
 
 ##displayInventory(stuff)
