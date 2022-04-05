b6dd3b35298a554d8c9bf98e527284e62a70b1b6
Tyler Paneitz <Tyler.Paneitz@vu.com>
2022-04-05 12:08:05 -0500

    Added inventory statement

--- displayInventory.py
+++ displayInventory.py
@@ -1,6 +1,11 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
-    # your code goes here
+    total = 0
+    print("Inventory:")
+    for key in stuff:
+        total += stuff[key]
+        print(str(stuff[key]) + " " + key)
+    print("Total number of items: " + str(total))
 
 ##displayInventory(stuff)
